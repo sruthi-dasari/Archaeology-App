5de0a419686543696b3bcd40ee7852b6e290b518
sruthi <sruthi@example.com>
2023-08-21 16:17:54 +0000

    in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from "react";
+
+import {
+  MainContainer,
+  DescriptionContainer,
+  MainHeading,
+  LikeShareViewContainer,
+  IconAndCountContainer,
+  IconContainer,
+  HeartIcon,
+  ShareIcon,
+  ViewIcon,
+  CountText,
+  ImageDisplayContainer,
+  KeyValueContainer,
+  KeyValuePair,
+  DescriptionHeading,
+  DescriptionText,
+  ReadMoreContainer,
+  ArrowIcon,
+  ReadMoreText,
+  ButtonsContainer,
+  BrownButton,
+  TransparentButton,
+} from "./styledComponents";
+
+class App extends Component {
+  render() {
+    console.log("In App.js render()");
+
+    return (
+      <MainContainer>
+        <DescriptionContainer>
+          <MainHeading>
+            MAITREYA BUDDHA IN GESTURE OF FEARLESSNESS (ABHAYA MUDRA)
+          </MainHeading>
+          <LikeShareViewContainer>
+            <IconAndCountContainer>
+              <IconContainer>
+                <HeartIcon></HeartIcon>
+              </IconContainer>
+              <CountText>0</CountText>
+            </IconAndCountContainer>
+            <IconAndCountContainer>
+              <IconContainer>
+                <ShareIcon></ShareIcon>
+              </IconContainer>
+              <CountText>120</CountText>
+            </IconAndCountContainer>
+            <IconAndCountContainer>
+              <IconContainer>
+                <ViewIcon></ViewIcon>
+              </IconContainer>
+              <CountText>27</CountText>
+            </IconAndCountContainer>
+          </LikeShareViewContainer>
+          <KeyValueContainer>
+            <KeyValuePair>Dynasty: Ahichchhatra</KeyValuePair>
+            <KeyValuePair>Period: 200 CE</KeyValuePair>
+            <KeyValuePair>Material: Sandstone</KeyValuePair>
+            <KeyValuePair>Location: National Museum</KeyValuePair>
+          </KeyValueContainer>
+          <DescriptionHeading>Description:</DescriptionHeading>
+          <DescriptionText>
+            Maitreya, the future Buddha to be, resides in the Tushita heaven as
+            a bodhisattva waiting to redeem humanity. In Buddhism, Maitreya is
+            the eighth Buddha, a successor of the seven historical
+            Buddhas(sapta-manushi Buddhas). The Digha Nikaya mentions, Maitreya
+            Buddha will be born in ketumati, in present-day Varanasi, Uttar
+            Pradesh. As a bodhisattva, Maitreya wears a heavily adorned with
+            earings, wristlets, necklaces and an amulet. The
+          </DescriptionText>
+          <ReadMoreContainer>
+            <ArrowIcon />
+            <ReadMoreText>Read More</ReadMoreText>
+          </ReadMoreContainer>
+          <ButtonsContainer>
+            <BrownButton type="button">ADD TO COLLECTIONS</BrownButton>
+            <TransparentButton type="button">SOUVENIR</TransparentButton>
+          </ButtonsContainer>
+        </DescriptionContainer>
+        <ImageDisplayContainer></ImageDisplayContainer>
+      </MainContainer>
+    );
+  }
 }
 
 export default App;
---
+++ src/styledComponents.js
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+
+import {
+  AiOutlineHeart,
+  AiOutlineShareAlt,
+  AiOutlineEye,
+  AiOutlineArrowRight,
+} from "react-icons/ai";
+
+export const MainContainer = styled.div`
+  display: flex;
+  padding: 40px;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  width: 30vw;
+  flex-direction: column;
+`;
+
+export const MainHeading = styled.h1`
+  display: flex;
+  font-family: "Roboto";
+  font-size: 25px;
+`;
+
+export const LikeShareViewContainer = styled.div`
+  display: flex;
+  width: 180px;
+  justify-content: space-between;
+`;
+
+export const IconAndCountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  background-color: #c6cdcf;
+  height: 35px;
+  width: 35px;
+  border-radius: 40px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeartIcon = styled(AiOutlineHeart)`
+  display: flex;
+`;
+
+export const ShareIcon = styled(AiOutlineShareAlt)`
+  display: flex;
+`;
+
+export const ViewIcon = styled(AiOutlineEye)`
+  display: flex;
+`;
+
+export const CountText = styled.p`
+  margin-top: 5px;
+  font-size: 17px;
+  color: black;
+  font-weight: 500;
+`;
+
+export const KeyValueContainer = styled.div``;
+
+export const KeyValuePair = styled.p`
+  color: #939696;
+  font-weight: 700;
+  font-size: 16px;
+  margin: 7px;
+  margin-left: 0px;
+`;
+
+export const DescriptionHeading = styled.h1`
+  font-family: "Roboto";
+  font-size: 18px;
+`;
+
+export const DescriptionText = styled.p`
+  color: #939696;
+  font-weight: 700;
+  font-size: 16px;
+  margin: 7px;
+  margin-left: 0px;
+`;
+
+export const ReadMoreContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ArrowIcon = styled(AiOutlineArrowRight)`
+  color: #ab6533;
+`;
+
+export const ReadMoreText = styled.h1`
+  font-family: "Roboto";
+  font-size: 18px;
+  color: #ab6533;
+`;
+
+export const ButtonsContainer = styled.div``;
+
+export const BrownButton = styled.button`
+  background-color: #ab6533;
+  color: #ffffff;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  font-weight: 800;
+  margin-right: 10px;
+  font-size: 12px;
+`;
+
+export const TransparentButton = styled.button`
+  background-color: #ffffff;
+  color: #ab6533;
+  padding: 10px;
+  border: 1px solid #ab6533;
+  border-radius: 5px;
+  font-weight: 800;
+  font-size: 12px;
+`;
+
+export const ImageDisplayContainer = styled.div`
+  //   background-image: linear-gradient(to left, #1c1c1b, #575350, #7a7572);
+  background-image: url("https://ibb.co/QfcsTLs");
+  background-size: cover;
+  width: 70vw;
+  height: 100vh;
+`;
